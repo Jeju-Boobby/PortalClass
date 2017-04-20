85829c3f2cd708dcd4448502c014b02bfa734091
Jeju-Boobby <boo1984@naver.com>
2017-04-20 16:13:20 +0900

    12. 람다표현식, 클래스 제거

--- userdao/src/kr/ac/jejunu/UserDao.java
+++ userdao/src/kr/ac/jejunu/UserDao.java
@@ -14,19 +14,36 @@ public class UserDao {
     }
 
     public User get(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new StatementStrategyForGet(id);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement;
+            preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        };
 
         return jdbcContext.jdbcContextWithStatementStrategyForGet(statementStrategy);
     }
 
     public void add(User user) throws SQLException {
-        StatementStrategy statementStrategy = new StatementStrategyForAdd(user);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo VALUES (?, ?, ?)");
+            preparedStatement.setLong(1, user.getId());
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getPassword());
+
+            return preparedStatement;
+        };
 
         jdbcContext.jdbcContextWithStatementStrategyForUpdate(statementStrategy);
     }
 
     public void delete(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new StatementStrategyForUpdate(id);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement;
+            preparedStatement = connection.prepareStatement("delete from userinfo WHERE id = ?");
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        };
 
         jdbcContext.jdbcContextWithStatementStrategyForUpdate(statementStrategy);
     }
