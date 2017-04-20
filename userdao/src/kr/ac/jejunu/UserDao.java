c5234fcf0ed76a41c9bd3de7d80d881f97705b3d
Jeju-Boobby <boo1984@naver.com>
2017-04-20 15:15:35 +0900

    1. add

---
+++ userdao/src/kr/ac/jejunu/UserDao.java
@@ -0,0 +1,46 @@
+package kr.ac.jejunu;
+
+import java.sql.*;
+
+/**
+ * Created by Boobby on 17-4-20.
+ */
+public class UserDao {
+    public User get(Long id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/portal?characterEncoding=utf-8", "root", "qkqh1125");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setLong(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getLong("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/portal?characterEncoding=utf-8", "root", "qkqh1125");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo VALUES (?, ?, ?)");
+        preparedStatement.setLong(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+}
